df7265f6e7b37004b7116372ed322d753afa1a9e
Josh Pester <jp2386@cornell.edu>
2023-09-26 19:52:53 -0400

    Created Defects.java file

---
+++ Defects.java
@@ -0,0 +1,52 @@
+import java.time.*;
+
+public class Defects {
+    
+    public int ID;
+    public String name;
+    public String description;
+    public LocalDateTime createdAt;
+    public String status;
+    public int createdBy;
+    public int assignedTo;
+
+    public void Defects(int ID, String name, String description,
+                        String status, int createdBy, int assignedTo) {
+            this.ID = ID;
+            this.name = name;
+            this.description = description;
+            this. createdAt = LocalDateTime.now();
+            this.status = status;
+            this.createdBy = createdBy;
+            this.assignedTo = assignedTo;
+    }
+
+    public int getID(){
+        return ID;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public int getCreatedBy(){
+        return createdBy;
+    }
+
+    public int getAssignedTo(){
+        return assignedTo;
+    }
+
+}
