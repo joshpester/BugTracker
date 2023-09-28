f79fe57043f6b09314dfd99d35ee50fb751e6e72
Josh Pester <jp2386@cornell.edu>
2023-09-27 20:41:03 -0400

    Connecetd Intro.java to mysql BugTrackerDB

--- Intro.java
+++ Intro.java
@@ -1,7 +1,31 @@
+import java.sql.*;
+
 public class Intro {
     
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Connection conn = null;
+
+        try {
+            String url = "jdbc:mysql://localhost:3306/BugTrackerDB";
+            String username = "root";
+            String password = "Pest2386!";
+
+            conn = DriverManager.getConnection(url, username, password);
+            if(conn != null) {
+                System.out.println("Connected to the database BugTrackerDB");
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error occurred");
+            ex.printStackTrace();
+        } finally {
+            if(conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
     }
 
 }
