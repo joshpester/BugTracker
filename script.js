f417fc68493ca60486186a0827bf7a062e4578fd
Josh Pester <jp2386@cornell.edu>
2023-10-06 17:59:50 -0400

    Adding to the Front-End and adding node.js

---
+++ script.js
@@ -0,0 +1,34 @@
+//Code for clicking on table entries
+$(document).ready(function () {
+    $('table tbody').on('click', 'tr', function() {
+        const id = $(this).data('id');
+    })
+});
+
+//Code for switching between different dashboard views
+
+//Create variables for references to dropdown and view contianers
+const viewSelector = document.getElementById('viewSelector');
+const dashboardViewContainer = document.getElementById('dashboardViewContainer');
+
+//Function to load contain based on the selected dashboard view
+function loadPage(view) {
+    console.log(view);
+    fetch(view)
+        .then(response => response.text())
+        .then(html => {
+            dashboardViewContainer.innerHTML = html;
+        })
+        .catch(error => {
+            console.error('Error loading page:', error);
+        });
+}
+
+//Add event listener to the dropdown
+viewSelector.addEventListener('change', function() {
+    const selectedView = viewSelector.value;
+    loadPage(selectedView);
+});
+
+//Load default view
+loadPage(viewSelector.value);
